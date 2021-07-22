79a0c6e8ed395fe85c5bff25014217d3ac73abf3
pavanyalala <pavanyalala4508@gmail.com>
2021-07-22 12:54:08 +0530

    [pavan]: points of two lines are initialized

---
+++ Geometry/LengthCalculation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    class LengthCalculation
+    {
+        int PointX1, PointX2, PointY1, PointY2;//variables
+        public LengthCalculation(int PointX1, int PointX2, int PointY1, int PointY2)//constructor to initialize variable
+        {
+            this.PointX1 = PointX1;
+            this.PointX2 = PointX2;
+            this.PointY1 = PointY1;
+            this.PointY2 = PointY2;
+
+        }
+        public double CalculateLength()//method calculate length of line
+        {
+            double length = Math.Sqrt((PointX2 - PointX1) ^ 2 + (PointY2 - PointY1) ^ 2);
+            return length;
+        }
+
+    }
+}
--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -7,6 +7,15 @@ namespace Geometry
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Program");
+
+            LengthCalculation lengthCalculation = new LengthCalculation(2, 4, 3, 9);
+            double line1 = lengthCalculation.CalculateLength();
+            Console.WriteLine("Length of Line one : " + line1);
+
+            LengthCalculation lengthCalculation1 = new LengthCalculation(3, 9, 4, 12);
+            double line2 = lengthCalculation1.CalculateLength();
+            Console.WriteLine("Length of Line Two : " + line2);
+
         }
     }
 }
