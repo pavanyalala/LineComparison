b49f11e919316ee402d89c4174536c44c1cc7dec
pavanyalala <pavanyalala4508@gmail.com>
2021-07-27 14:20:30 +0530

    [pavan]:Compared Two Lines Using Class Methods

---
+++ Geometry/Comparission.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    class Comparission
+    {
+        public void ComparingLines()
+        {
+            LengthCalculation line1 = new LengthCalculation(1, 2, 3, 4);
+            LengthCalculation line2 = new LengthCalculation(1, 2, 3, 4);
+            int line1Length = Convert.ToInt32(line1.CalculateLength());
+            int line2Length = Convert.ToInt32(line2.CalculateLength());
+            if (line1Length.CompareTo(line2Length) > 0)
+                Console.WriteLine("Line 1 is Greaterthan Line 2");
+            else if (line1Length.CompareTo(line2Length) < 0)
+                Console.WriteLine("Line 1 is Lessthan Line 2");
+            else
+                Console.WriteLine("Both the Lines Are Equal");
+        }
+    }
+}
--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -8,20 +8,9 @@ namespace Geometry
         {
             Console.WriteLine("Welcome to Line Comparison Program");
 
-            LengthCalculation lengthCalculation = new LengthCalculation(2, 4, 3, 9);
-            double line1 = lengthCalculation.CalculateLength();
-            Console.WriteLine("Length of Line one : " + line1);
+            Comparission comparission = new Comparission();
+            comparission.ComparingLines();
 
-            LengthCalculation lengthCalculation1 = new LengthCalculation(3, 9, 4, 12);
-            double line2 = lengthCalculation1.CalculateLength();
-            Console.WriteLine("Length of Line Two : " + line2);
-
-            if (line1.CompareTo(line2) > 0)
-                Console.WriteLine("Line1 is Greater than Line2");
-            else if (line1.CompareTo(line2) < 0)
-                Console.WriteLine("Line1 is Less than Line2");
-            else
-                Console.WriteLine("Both Line1 and Line2 Are Equal");
         }
     }
 }
